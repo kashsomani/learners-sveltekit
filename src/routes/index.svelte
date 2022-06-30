7ccb482d9b71439e976357a34316409b4bf3b6ab
Kash Somani <kash@kashsomani.com>
2022-06-29 21:46:48 -0700

    changed theme and added text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+    import logo from "$lib/assets/old_logo.png";
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <div class="grid grid-cols-1 place-items-center">
+                <img src={logo} alt="logo" width="50%" />
+            </div>
+            <h1 class="text-5xl font-bold text-primary">Learners' Academy</h1>
+            <p class="py-6">
+                Learners’ Academy was established in June 1983 with only
+                Pre-Primary and Primary sections. Today, we have classes up to
+                Std. X (I.C.S.E.). We have grown with our children to give our
+                community a home where learning begins and from where emerge the
+                citizens of tomorrow. We have cutting-edge technologies coupled
+                with experienced teachers to cater to the intrinsic as well as
+                extrinsic devlopments of all children. With STEM being our
+                primary focus, our alumni have consistently gone forward to work
+                in various industries such as medicine, engineering, technology
+                and many more
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,28 @@ const config = {
   },
   daisyui: {
     themes: [
-      "garden"
+      {
+        mytheme: {
+
+          "primary": "#16a34a",
+
+          "secondary": "#ECF4E7",
+
+          "accent": "#FAE5E5",
+
+          "neutral": "#5D5656",
+
+          "base-100": "#E9E7E7",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
     ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
