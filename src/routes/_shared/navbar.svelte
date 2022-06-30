a72622c44730e22119503509cc3799b487471099
Kash Somani <kash@kashsomani.com>
2022-06-29 21:51:07 -0700

    changed navbar styles and made sticky

--- src/routes/_shared/navbar.svelte
+++ src/routes/_shared/navbar.svelte
@@ -1,37 +1,39 @@
-<div class="navbar bg-base-100">
-    <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-5 h-5 stroke-current"
-                ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                /></svg
+<script>
+    import logo from "$lib/assets/logo.png";
+</script>
+
+<div class="navbar bg-base-100 sticky top-0 z-20">
+    <div class="navbar-start">
+        <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost btn-circle">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    ><path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h7"
+                    /></svg
+                >
+            </label>
+            <ul
+                tabindex="0"
+                class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-        </button>
+                <li><a>Homepage</a></li>
+                <li><a>Portfolio</a></li>
+                <li><a>About</a></li>
+            </ul>
+        </div>
     </div>
-    <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div class="navbar-center">
+        <a class="btn btn-ghost normal-case text-xl text-primary"
+            >Learners' Academy</a
+        >
     </div>
-    <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-5 h-5 stroke-current"
-                ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                /></svg
-            >
-        </button>
-    </div>
+    <div class="navbar-end" />
 </div>
