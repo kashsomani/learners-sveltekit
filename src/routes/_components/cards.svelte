3b3fca3989f5f341d64287dccc2c8d23daea38af
Kash Somani <kash@kashsomani.com>
2022-07-09 19:54:33 -0700

    refactored

---
+++ src/routes/_components/cards.svelte
@@ -0,0 +1,14 @@
+<div class="grid grid-cols-1 place-items-center">
+    <div class="card card-side bg-lime-200 shadow-xl">
+        <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Watch</button>
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,35 +1,11 @@
 <script>
     import Carousel from "./_components/carousel.svelte";
+    import Cards from "./_components/cards.svelte";
+    import Hero from "./_components/hero.svelte";
 </script>
 
 <div id="main">
-    <div class="hero min-h-screen">
-        <div class="hero-content text-center">
-            <div class="max-w-md">
-                <div class="grid grid-cols-1 place-items-center">
-                    <img src="/assets/old_logo.png" alt="logo" width="50%" />
-                </div>
-                <h1 class="text-5xl font-bold text-primary">
-                    Learners' Academy
-                </h1>
-                <p class="py-6 text-justify">
-                    Learners’ Academy was established in June 1983 with only
-                    Pre-Primary and Primary sections. Today, we have classes up
-                    to Std. X (I.C.S.E.). We have grown with our children to
-                    give our community a home where learning begins and from
-                    where emerge the citizens of tomorrow. We have cutting-edge
-                    technologies coupled with experienced teachers to cater to
-                    the intrinsic as well as extrinsic devlopments of all
-                    children. With STEM being our primary focus, our alumni have
-                    consistently gone forward to work in various industries such
-                    as medicine, engineering, technology and many more
-                </p>
-                <!-- maybe add something like this in future -->
-                <!-- <button class="btn btn-primary">Get Started</button> -->
-            </div>
-        </div>
-    </div>
-
+    <Hero />
     <Carousel />
     <div class="grid grid-cols-1 place-items-center">
         <p class="py-6 mx-28 text-justify">
@@ -67,6 +43,7 @@
             environment to its students.
         </p>
     </div>
+    <Cards />
 </div>
 
 <style lang="postcss">
